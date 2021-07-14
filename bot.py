643048e203dec94f89e9ad2aefc7af4f22be9844
stas <58526725+Seleznyov@users.noreply.github.com>
2021-07-14 18:45:26 +0300

    #3

--- bot.py
+++ bot.py
@@ -3,6 +3,19 @@ from TelegramBot.method import sendMessage, getUpdates
 import requests
 
 okCodes = (200,201)
+def allRates(name):
+    res = requests.get(url_Cur)
+    result = res.json()
+    for i in result:
+        if i["Cur_Abbreviation"] == name:
+            return i["Cur_OfficialRate"]
+
+allcurrenciesList = []
+def allcurrencies():
+    res = requests.get(url_Cur)
+    result = res.json()
+    for i in result:
+        allcurrenciesList.append(i["Cur_Abbreviation"])
 
 def getBotUpdates(token):
     res = requests.get(root_url_bot+token+getUpdates)
@@ -21,19 +34,40 @@ def sendMesage(mesage_text, id_chat):
         return False
 
 prev_update_id = 0
+allcurrencies()
 while True:
     res = getBotUpdates(TOKEN)
     last_update_id = res["result"][-1]["update_id"]
     if last_update_id > prev_update_id:
         chat_id = res["result"][-1]["message"]["chat"]["id"]
-        message = "xxx"
-        sendMesage(message, chat_id)
-        prev_update_id = last_update_id
+        last_update_text = res["result"][-1]["message"]["text"]
+        for nameCur in allcurrenciesList:
+            if nameCur in last_update_text:
+                message = nameCur
+                sendMesage(str(allRates(message)), chat_id)
+        # else:
+        #     message = "sorry try again"
+        #     sendMesage(message, chat_id)
+    prev_update_id = last_update_id
 
+
+# allRates("BGN")
 
 res = getBotUpdates(TOKEN)
 print(res)
+
+# allcurrencies()
+# print(allcurrenciesList)
+
+# last_update_id = res["result"][-1]["message"]["text"]
+# print(last_update_id)
+
 # chat_id = res["result"][-1]["message"]["chat"]["id"]
 # message = "xxx"
 # sendMesage(message,chat_id)
 
+# list=['AUD', 'AMD', 'BGN', 'UAH',
+#       'DKK', 'USD', 'EUR', 'PLN', 'JPY',
+#       'IRR', 'ISK', 'CAD', 'CNY', 'KWD',
+#       'MDL', 'NZD', 'NOK', 'RUB', 'XDR', 'SGD',
+#       'KGS', 'KZT', 'TRY', 'GBP', 'CZK', 'SEK', 'CHF']
